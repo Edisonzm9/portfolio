7c1ae7979f108ced5bec9fcd8f37c3cd5d378a0f
edisonzm9 <eazamoram99@gmail.com>
2024-09-25 18:16:56 -0500

    update: Carousel

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,8 +6,12 @@ import c1 from '../../assets/images/c1.png';
 import c2 from '../../assets/images/c2.png';
 import c3 from '../../assets/images/c3.png';
 import c4 from '../../assets/images/c4.png';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 // Importación del carrusel de react-bootstrap
 import Carousel from 'react-bootstrap/Carousel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
 
 const AboutMe = () => {
   return (
@@ -29,10 +33,10 @@ const AboutMe = () => {
           transition={{ duration: 2 }}
         >
           <h1>About me...</h1>
-          <p>Software engineer graduated from the Universidad Técnica del Norte, specializing in web, mobile, and desktop development. 
-            Focused on creating efficient and scalable solutions using technologies such as Angular, React, relational and non-relational 
-            databases, and Python. Currently deepening knowledge in React and exploring data science. Passionate about problem-solving 
-            and building products that positively impact users. Learning new tools and collaborating in multidisciplinary teams are key 
+          <p>Software engineer graduated from the Universidad Técnica del Norte, specializing in web, mobile, and desktop development.
+            Focused on creating efficient and scalable solutions using technologies such as Angular, React, relational and non-relational
+            databases, and Python. Currently deepening knowledge in React and exploring data science. Passionate about problem-solving
+            and building products that positively impact users. Learning new tools and collaborating in multidisciplinary teams are key
             motivations.</p>
         </motion.div>
       </div>
@@ -78,6 +82,49 @@ const AboutMe = () => {
           </Carousel>
         </div>
       </div>
+
+      {/* Línea temporal de experiencia */}
+      <div className="experience-section">
+        <motion.h2
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+        >
+          My Professional Experience
+        </motion.h2>
+
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: '#f9f9f9', color: '#333' }}
+            contentArrowStyle={{ borderRight: '7px solid #3498db' }} // Flecha azul
+            date="2024 - Present"
+            iconStyle={{ background: '#3498db', color: '#fff' }} // Fondo azul, icono blanco
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Developer - Reparauto Ibarra</h3>
+            <p>
+              Development of the company website and automotive management ERP using Angular.js and Node.js to optimize internal processes like inventory control and customer management.
+            </p>
+          </VerticalTimelineElement>
+
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: '#f9f9f9', color: '#333' }}
+            contentArrowStyle={{ borderRight: '7px solid #e74c3c' }} // Flecha roja
+            date="2022"
+            iconStyle={{ background: '#e74c3c', color: '#fff' }} // Fondo rojo
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Intern - Plus Servicios Tecnológicos</h3>
+            <p>
+              Worked on internal management modules using technologies like MongoDB, PostgreSQL, and PHP, optimizing internal processes.
+            </p>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+
+
     </div>
   );
 };
