ea15bb060c6447e7e47d54820561825337b8763f
edisonzm9 <eazamoram99@gmail.com>
2024-10-01 22:17:17 -0500

    u

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,25 +2,25 @@ import React from 'react';
 import './Projects.css';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-
-
-
+import p1 from '../../assets/images/p1.png';
 
 const Projects = () => {
     return (
         <div className='cards'>
-            <div>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>Web Page - Reparauto Ibarra</Card.Title>
-                        <Card.Text>
-                            In this project I build a web page to Reparauto Ibarra. This web page contain information about process
-                        </Card.Text>
-                        <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </div>
+            <Card className='card'>
+                <Card.Img variant="top" src={p1} />
+                <Card.Body>
+                    <Card.Title>Web Page - Reparauto Ibarra</Card.Title>
+                    <Card.Text>
+                        In this project, I built a web page for Reparauto Ibarra. This web page was developed using Angular.js.
+                    </Card.Text>
+                    <div className="d-flex justify-content-end">
+                        <a href="https://reparauto-ibarra.web.app/" target="_blank" rel="noopener noreferrer">
+                            <Button variant="primary">Go to project</Button>
+                        </a>
+                    </div>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
